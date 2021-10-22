3f2fc7f38305fbd6883ea33933bdc21dcd4c7de5
Jonathan Gobiel <jonathangobiel13@gmail.com>
2021-10-22 20:50:57 +0700

    get latest stimulus at cartcontroller

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -14,7 +14,7 @@ class CartController extends Controller
     }
     public function index()
     {
-        $carts = Cart::with(['User.StimulusMap.Stimulus', 'Training'])->where('user_id', Auth::id())->get();
+        $carts = Cart::with(['User.StimulusMap.Stimulus.Training', 'Training'])->where('user_id', Auth::id())->get();
         return (json_encode($carts));
         return view('cart', compact('carts'));
     }
--- app/Models/User.php
+++ app/Models/User.php
@@ -48,7 +48,7 @@ class User extends Authenticatable
     ];
 
     public function StimulusMap(){
-        return $this->hasOne(StimulusMap::class, 'user_id');
+        return $this->hasMany(StimulusMap::class, 'user_id')->latest()->first();
     }
 
     public function isAdmin(){
