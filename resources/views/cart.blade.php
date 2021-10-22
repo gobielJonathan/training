f9343cef851f48280762dc9ecd09d3b21d927801
Jonathan Gobiel <jonathangobiel13@gmail.com>
2021-10-22 22:41:44 +0700

    set layout cart

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -32,11 +32,8 @@
                                 @endphp
                                 <h3 class="text-bold">{{$ls->training->title}}</h3>
                                 <h5>{{$ls->name}}</h5>
-                                <div class="d-flex">
-                                    <div class="col-3"><p>Rp.{{$ls->training->price}} </div>
-
-                                    <div class="col-9">
-                                          <div class="input-group">
+                                <p>Rp.{{$ls->training->price}} <br>
+                                <div class="input-group">
                                     <span class="input-group-prepend">
                                         <button type="button" class="btn btn-sm"  disabled="disabled" data-type="minus" data-id="{{$item->id}}">
                                             <span class="fa fa-minus"></span>
@@ -48,17 +45,11 @@
                                             <span class="fa fa-plus"></span>
                                         </button>
                                     </span>
-                                </div>
-                                    </div>
-                                </p>
                                 </div>
                                @elseif($item->training)
                                 <h3 class="text-bold">{{$item->training->title}}</h3>
-                                <div class="d-flex">
-                                    <div class="col-3"><p>Rp.{{$ls->training->price}} </div>
-
-                                    <div class="col-9">
-                                          <div class="input-group">
+                                <p>Rp.{{$ls->training->price}}</p> <br>
+                                <div class="input-group">
                                     <span class="input-group-prepend">
                                         <button type="button" class="btn btn-sm"  disabled="disabled" data-type="minus" data-id="{{$item->id}}">
                                             <span class="fa fa-minus"></span>
@@ -70,9 +61,6 @@
                                             <span class="fa fa-plus"></span>
                                         </button>
                                     </span>
-                                </div>
-                                    </div>
-                                </p>
                                 </div>
                                @endif
                            </div>
