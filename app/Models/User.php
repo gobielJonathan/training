489bc3bb786873e4f4a98c614c5b8fc03a0e5b86
Jonathan Gobiel <jonathangobiel13@gmail.com>
2021-10-22 20:52:05 +0700

    remove get latest stimulus at cartcontroller

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,7 +48,7 @@ class User extends Authenticatable
     ];
 
     public function StimulusMap(){
-        return $this->hasMany(StimulusMap::class, 'user_id')->latest()->first();
+        return $this->hasMany(StimulusMap::class, 'user_id');
     }
 
     public function isAdmin(){
