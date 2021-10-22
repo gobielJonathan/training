96e3231a1fc5823927beda2593771cc9c5a8b9bd
Jonathan Gobiel <jonathangobiel13@gmail.com>
2021-10-22 21:53:21 +0700

    set layouting cart

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -8,31 +8,33 @@
     </h1>
 
     <div class="row mt-5">
-        <div class="col-sm-12 col-md-7">
+        <div class="col-md-12 col-lg-8">
 
            @if (count($carts) > 0)
            @foreach ($carts as $item)
            <div class="p-4 box-shadow box-rounded mb-4 cart-item">
                <div class="row">
-                   <div class="col-sm-12 col-md-10">
+                   <div class="col-md-12 col-lg-10">
                        <div class="d-flex">
-                           @if(isset($item->user->stimulus_map_on_going))
-                           <img src="{{$item->user->stimulus_map_on_going->stimulus->training->image}}" class="box-rounded" alt="banner-thumbnail"
-                               style="width: 400px;height: 400px;">
+                           <div class="col-md-12 col-lg-8">
+                               @if(isset($item->user->stimulus_map_on_going))
+                                <img src="{{$item->user->stimulus_map_on_going->stimulus->training->image}}" class="box-rounded" alt="banner-thumbnail"
+                                style="width: 400px;height: 400px;">
                             @else
-                            <img src="{{$item->training->image}}" class="box-rounded" alt="banner-thumbnail"
-                               style="width: 400px;height: 400px;">
+                                <img src="{{$item->training->image}}" class="box-rounded" alt="banner-thumbnail"
+                                style="width: 400px;height: 400px;">
                             @endif
-                           <div class=" ml-3">
+                           </div>
+                           <div class="col-md-12 col-lg-4">
                                @if(isset($item->user->stimulus_map_on_going))
                                 @php
                                     $ls = $item->user->stimulus_map_on_going->stimulus;
                                 @endphp
-                                <h2 class="text-bold">{{$ls->training->title}}</h2>
-                                <h4>{{$ls->name}}</h4>
+                                <h3 class="text-bold">{{$ls->training->title}}</h3>
+                                <h5>{{$ls->name}}</h5>
                                 <p>Rp.{{$ls->training->price}} x {{$item->total}}</p>
                                @elseif($item->training)
-                                <h2 class="text-bold">{{$item->training->title}}</h2>
+                                <h3 class="text-bold">{{$item->training->title}}</h3>
                                 <p>Rp.{{$item->training->price}} x {{$item->total}}</p>
                                @endif
                            </div>
@@ -55,7 +57,7 @@
 
 
         </div>
-        <div class="col-sm-12 col-md-5">
+        <div class="col-md-12 col-lg-4">
             @php
             $total_price = 0;
             foreach ($carts as $item) {
